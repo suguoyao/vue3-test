a6ee51a2272bfc5a6227a70ee0fc6eaffb96d0cf
suguoyao <suguoyao888@gmail.com>
2020-04-22 16:59:59 +0800

    Reactivity APIs, Lifecycle Hooks, Template Refs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="hello">
-    <h1>Welcome to {{ username }}</h1>
-    <button @click="inc">Clicked {{ count }} times.</button>
+    <h1>{{ username }}!</h1>
+    <p>{{ description }}</p>
+    <button @click="increment">Clicked {{ count }} times.</button>
+    <button @click="showPanelHandle">Show Panel</button>
+
+    <div v-if="showPanel" ref="panelDomRef">Panel content</div>
   </div>
 </template>
 
 <script>
-  import {ref, watchEffect} from 'vue'
+  import {
+    ref, reactive, watchEffect, computed, readonly,
+    onMounted, onUpdated, onUnmounted
+  } from 'vue'
 
   export default {
     name: 'HelloWorld',
@@ -21,26 +28,92 @@
       console.log('setup context', context)
 
       const count = ref(0)
+      const copyCount = readonly(count)
       const numberRef = ref(1)
       const stringRef = ref('a')
+      const showPanel = ref(false)
+      const panelDomRef = ref(null)
 
-      console.log(numberRef.value)
+      console.log(numberRef.value) // output: 1
       numberRef.value++
-      console.log(numberRef.value)
+      console.log(numberRef.value) // output: 2
+      console.log(stringRef) // output: {value: "a", _isRef: true}
 
-      console.log(stringRef) // ouput: {value: "a", _isRef: true}
-
-      watchEffect(() => {
-        console.log(`name is: ` + props.username)
+      const state = reactive({
+        count
       })
 
-      const inc = () => {
+      console.log(state.count) //  output: 0
+      state.count = 1
+      console.log(count.value) //  output: 1
+
+      // if a new ref is assigned to a property linked to an existing ref
+      // it will replace the old ref
+      state.count = ref(8)
+      console.log(state.count) //  output: 8
+      console.log(count.value) //  output: 1
+
+      const isEven = computed(() => count.value % 2 === 0)
+
+      const increment = () => {
         count.value++
+        console.log(`Is count an even number ? ${isEven.value}`)
+
+        copyCount.value++ // Warning! Set operation on key "value" failed: target is readonly
       }
+
+      const showPanelHandle = () => {
+        showPanel.value = !showPanel.value
+      }
+
+      watchEffect(() => {
+        console.log(`watchEffect: name is ${props.username}`)
+        console.log(count.value)
+      })
+
+      /**
+       beforeCreate -> use setup()
+       created -> use setup()
+       beforeMount -> onBeforeMount
+       mounted -> onMounted
+       beforeUpdate -> onBeforeUpdate
+       updated -> onUpdated
+       beforeDestroy -> onBeforeUnmount
+       destroyed -> onUnmounted
+       errorCaptured -> onErrorCaptured
+       */
+      onMounted(() => {
+        console.log('mounted!')
+        watchEffect(() => {
+          // access the DOM or template refs
+        })
+      })
+      onUpdated(() => {
+        console.log('updated!')
+        console.log('panelDomRef', panelDomRef.value)
+        if (panelDomRef.value) {
+          console.log(panelDomRef.value.textContent)
+        }
+      })
+      onUnmounted(() => {
+        console.log('unmounted!')
+      })
+
       return {
         count,
-        inc
+        description: ref('This is the description.'),
+        showPanel,
+        panelDomRef,
+        increment,
+        showPanelHandle
       }
+    },
+    // new hooks
+    onRenderTracked(e) {
+      console.log(e)
+    },
+    onRenderTriggered(e) {
+      console.log(e)
     }
   }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld username="SUGAR"/>
+    <HelloWorld :username="username"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
+  import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  import {ref} from 'vue'
+
+  export default {
+    name: 'Home',
+    components: {
+      HelloWorld
+    },
+    setup() {
+      const username = ref('SUGAR')
+
+      setTimeout(() => {
+        // Will trigger the watchEffect method of HelloWorld component
+        username.value = "SSSSS"
+      }, 1000)
+
+      return {
+        username
+      }
+    }
   }
-}
 </script>
